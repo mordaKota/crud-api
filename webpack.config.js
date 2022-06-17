e947c2213926fe9e4577682d0037a87fa2be8fa6
Ekaterina Danilova <danilova.ek.an@gmail.com>
2022-06-17 15:28:14 +0300

    init: start crud-api tast, set up configs

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+console.log('IS DEV:', isDev);
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  entry: './index.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', 'json'],
+    alias: {
+      '@modules': path.resolve(__dirname, 'src/modules'),
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new ESLintPlugin(),
+  ],
+};
