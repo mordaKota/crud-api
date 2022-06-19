2f986468d031dfc0644660704d1961a6fb027328
Ekaterina Danilova <danilova.ek.an@gmail.com>
2022-06-19 21:28:37 +0300

    doc: update README.md

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@ import cluster from 'cluster';
 import 'dotenv/config';
 import { port } from './utils';
 import server from "./server";
+import os from 'os';
 
-const numCPUs = 4; // todo
+const numCPUs = os.cpus().length;
 
 if (cluster.isPrimary && process.env.MULTI === 'true') {
   for (let i = 0; i < numCPUs; i++) {
+    console.log(`Forking process number ${i + 1}...`);
     cluster.fork();
   }
 } else {
